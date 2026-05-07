95afcd993676ce211042f3a3d9bcf126a0e83906
solojoedev <216359848+solojoedev@users.noreply.github.com>
2026-05-07 16:06:36 -0400

    update zoe chatbot

--- app.py
+++ app.py
@@ -52,23 +52,21 @@ st.components.v1.html("""
     <html>
     <head>
         <script type="text/javascript">
-          (function(d, t) {
-              var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-              v.onload = function() {
-                window.voiceflow.chat.load({
-                  verify: { projectID: '698fc02dd6a662d6c77c4cdb' },
-                  url: 'https://general-runtime.voiceflow.com',
-                  versionID: 'production',
-                  voice: {
-                    url: "https://runtime-api.voiceflow.com"
-                  }
-                });
-              }
-              v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; 
-              v.type = "text/javascript"; 
-              s.parentNode.insertBefore(v, s);
-          })(document, 'script');
-        </script>
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '698fc02dd6a662d6c77c4cdb' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
     </head>
     <body style="margin:0; padding:0;">
     </body>
